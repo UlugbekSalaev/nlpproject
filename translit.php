78e658ce895bb39824839f5877815203653d65c5
Ulugbek Salaev <82821757+UlugbekSalaev@users.noreply.github.com>
2022-04-22 18:02:27 +0500

    Add files via upload

--- translit.php
+++ translit.php
@@ -7,13 +7,13 @@
 				<div class="col-sm-6">
 					<div class="btn-group btn-group-toggle" data-toggle="buttons">
 					  <label class="btn btn-secondary active">
-						<input type="radio" name="options" id="option_a1" autocomplete="off" checked=""> Lotin
+						<input type="radio" name="options" id="option_a1" autocomplete="off" checked=""> Latin
 					  </label>
 					  <label class="btn btn-secondary">
-						<input type="radio" name="options" id="option_a2" autocomplete="off"> Kirill
+						<input type="radio" name="options" id="option_a2" autocomplete="off"> Cyrillic
 					  </label>
 					  <label class="btn btn-secondary">
-						<input type="radio" name="options" id="option_a3" autocomplete="off"> Lotin N
+						<input type="radio" name="options" id="option_a3" autocomplete="off"> New Latin
 					  </label>
 					</div>
 					<textarea class="form-control" rows="6" placeholder="Enter ..."></textarea>
@@ -21,13 +21,13 @@
 				<div class="col-sm-6">
 					<div class="btn-group btn-group-toggle" data-toggle="buttons">
 					  <label class="btn btn-secondary active">
-						<input type="radio" name="options" id="option_a1" autocomplete="off" > Lotin
+						<input type="radio" name="options" id="option_a1" autocomplete="off" > Latin
 					  </label>
 					  <label class="btn btn-secondary">
-						<input type="radio" name="options" id="option_a2" autocomplete="off" checked=""> Kirill
+						<input type="radio" name="options" id="option_a2" autocomplete="off" checked=""> Cyrillic
 					  </label>
 					  <label class="btn btn-secondary">
-						<input type="radio" name="options" id="option_a3" style="padding:50px" autocomplete="off"> Lotin N
+						<input type="radio" name="options" id="option_a3" style="padding:50px" autocomplete="off"> New Latin
 					  </label>
 					</div>
 					<textarea class="form-control" rows="6" placeholder="Transliteration"></textarea>
