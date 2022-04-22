96010440c3adfd76e02a80a3492b020944df7ae4
Ulugbek Salaev <82821757+UlugbekSalaev@users.noreply.github.com>
2022-04-22 17:45:51 +0500

    Add files via upload

---
+++ translit.php
@@ -0,0 +1,42 @@
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+		<div class="card card-primary card-outline">
+          <div class="card-body">
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="btn-group btn-group-toggle" data-toggle="buttons">
+					  <label class="btn btn-secondary active">
+						<input type="radio" name="options" id="option_a1" autocomplete="off" checked=""> Lotin
+					  </label>
+					  <label class="btn btn-secondary">
+						<input type="radio" name="options" id="option_a2" autocomplete="off"> Kirill
+					  </label>
+					  <label class="btn btn-secondary">
+						<input type="radio" name="options" id="option_a3" autocomplete="off"> Lotin N
+					  </label>
+					</div>
+					<textarea class="form-control" rows="6" placeholder="Enter ..."></textarea>
+				</div>
+				<div class="col-sm-6">
+					<div class="btn-group btn-group-toggle" data-toggle="buttons">
+					  <label class="btn btn-secondary active">
+						<input type="radio" name="options" id="option_a1" autocomplete="off" > Lotin
+					  </label>
+					  <label class="btn btn-secondary">
+						<input type="radio" name="options" id="option_a2" autocomplete="off" checked=""> Kirill
+					  </label>
+					  <label class="btn btn-secondary">
+						<input type="radio" name="options" id="option_a3" style="padding:50px" autocomplete="off"> Lotin N
+					  </label>
+					</div>
+					<textarea class="form-control" rows="6" placeholder="Transliteration"></textarea>
+				</div>
+			</div>
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
